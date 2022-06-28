aaecedd88726003b1cdf7bd8aa38597b04ccbb4b
Darlan Souza Santos Cruz <100162578+Dansouza911@users.noreply.github.com>
2022-06-27 23:55:49 -0300

    final

---
+++ exercicio1.ts
@@ -0,0 +1,26 @@
+interface User {
+    name: string;
+    age: number;
+    occupation: string;
+  
+};
+
+export const users: unknown[] = [
+    {
+        name: 'Wilker',
+        age: 25,
+        occupation: 'Backend developer'
+    },
+    {
+        name: 'Bob',
+        age: 23,
+        occupation: 'Product Manager'
+    }
+];
+
+export function logPerson(user: unknown) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);
